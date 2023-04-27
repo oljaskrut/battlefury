e362a3b45739c6e3e95cf6dfd696e9faf785fe7d
Olzhas <oljaskrutf@gmail.com>
2023-04-27 04:50:28 +0000

    fixator

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,5 @@
 // remove this if you do not want your pages to be prerendered
 export const prerender = true
+import { dev } from "$app/environment"
+import { inject } from "@vercel/analytics"
+inject({ mode: dev ? "development" : "production" })
